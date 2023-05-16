4a2f8e588b76bac22a3f502bd2cd1c20a24258c7
italocirillo <italocirillo@gmail.com>
2023-05-16 20:40:49 +0200

    bonus 1 fatto

--- Models/Movie.php
+++ Models/Movie.php
@@ -7,6 +7,7 @@ class Movie
     public $titolo;
     public $descrizione;
     public $cover;
+    public $generi = [];
 
     //Costruttore
     function __construct($_titolo)
@@ -16,18 +17,26 @@ class Movie
 
     //Metodi
 
-    public function setData($_descrizione, $_cover)
+    public function setData($_descrizione, $_cover, $_generi)
     {
         $this->descrizione = $_descrizione;
         $this->cover = $_cover;
+        $this->generi = $_generi;
     }
 
     public function printMovie()
     {
-        return "
+        $caratteristicheMovie = "";
+        $caratteristicheMovie .= "
         Titolo: {$this->titolo},</br>
         Descrizione: {$this->descrizione}</br>
         Cover: {$this->cover}</br>
+        Generi: 
         ";
+        foreach ($this->generi as $genere) {
+            $caratteristicheMovie .= $genere . " ";
+        }
+
+        return $caratteristicheMovie;
     }
 }
--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 require_once __DIR__ . "/Models/Movie.php";
 
 $forrest_gump = new Movie("forrest gump");
-$forrest_gump->setData("lorem ipsum", "cover");
+$forrest_gump->setData("lorem ipsum", "cover", ["tragedy", "comedy"]);
 
 $guarda_in_alto = new Movie("guarda_in_alto");
-$guarda_in_alto->setData("lorem ipsum", "cover");
+$guarda_in_alto->setData("lorem ipsum", "cover", ["fantasy", "adventure"]);
 
 echo ($forrest_gump->printMovie());
-echo ("</br>");
+echo ("</br></br>");
 echo ($guarda_in_alto->printMovie());
